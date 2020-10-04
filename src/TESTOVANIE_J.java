32db588b4c6d147a946d80c4780dd1dec8abca79
Julia Svagrovska <jullina@192.168.1.14>
2020-10-04 16:04:53 +0200

    Initial commit of project.

---
+++ src/TESTOVANIE_J.java
@@ -0,0 +1,12 @@
+
+public class TESTOVANIE_J {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println( Math.pow(3, 2));
+System.out.println( Math.exp(2));
+
+//...
+	}
+
+}
